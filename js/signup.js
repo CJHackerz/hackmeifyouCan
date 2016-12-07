5edd8c42346d25cfdb1e7f03833d06038430acd0
Chirag Jariwala <sc.cjhackerz@yahoo.com>
2016-12-07 06:49:59 +0000

    Work done in signup.js file

--- js/signup.js
+++ js/signup.js
@@ -1,10 +1,24 @@
 function Validatesignup(){
     var name = document.getElementById("name").value;
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
     if(name == "") {
       alert("Error: Name cannot be blank!");
       document.getElementById("name").focus();
       document.getElementById("div_name").className = "form-group has-error";//Bootstrap error class
       return false;
+    }
+    if(username == ""){
+        alert("Error: Username cannot be blank!")
+        document.getElementById("username").focus();
+        document.getElementById("div_username").className = "form-group has-error";
+        return false;
+    }
+    if(password == ""){
+        alert("Error: Password cannot be blank!")
+        document.getElementById("password").focus();
+        document.getElementById("div_password").className = "form-group has-error";
+        return false;
     }
     else{
         return true;
