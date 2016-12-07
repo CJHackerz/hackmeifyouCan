e3a6197db2a4ae1e842d1939b74e394a11b1f4a7
Chirag Jariwala <sc.cjhackerz@yahoo.com>
2016-12-07 06:31:11 +0000

    signup.js working for name field

--- js/signup.js
+++ js/signup.js
@@ -1,9 +1,12 @@
-function signupValidate(){
-    var username = document.getElementById('username').value;
-    if(username == "") {
-      alert("Error: Username cannot be blank!");
-      document.getElementById('username').focus();
-      document.getElementById('username').className += "has-error";
+function Validatesignup(){
+    var name = document.getElementById("name").value;
+    if(name == "") {
+      alert("Error: Name cannot be blank!");
+      document.getElementById("name").focus();
+      document.getElementById("div_name").className = "form-group has-error";//Bootstrap error class
       return false;
-}
+    }
+    else{
+        return true;
+    }
 }
